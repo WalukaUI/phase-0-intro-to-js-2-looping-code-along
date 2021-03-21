fa0c7658decf7ebb5f0e40116d027c3de4615a75
WalukaUI <cwaluka@yahoo.com>
2021-03-20 23:54:29 -0700

    Tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+let spy;
+const all=['Lisa', 'Kaitlin', 'Jan'];
+const messages=[];
+
+beforeEach( () => {
+  spy = sinon.spy( console, 'log' );
+} );
+
+function writeCards(names,event){
+for(let i=0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`,);
+}
+return messages;
+}
+writeCards(all,'surprise');
+
+afterEach( () => {
+  spy.restore();
+} );
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function countDown(numProvided) {
+ for(let i = numProvided;i>=0;i--){
+   console.log(i);
+  }
+}
+
+
+  
